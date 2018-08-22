0283434855b5813f1972e89d52cbafe0ef9032a9
Kim Pham <pham.kim.a@gmail.com>
2018-08-22 15:56:32 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-  array.join(", )")
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    return array.join(" and ")
+  elsif array.size == 3
+    return
+  else
+    array.join(", )")
+  end
 end
